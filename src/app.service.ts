abf3c22ebff622a48bf40e4fbcbba4da6319cfa3
NanoThecnolog <contato@ericssongomes.com>
2025-02-15 18:06:05 -0300

    Ajusta respostas das rotas, aplicando padronização

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,6 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class AppService {
@@ -26,12 +27,21 @@ export class AppService {
 
   async setData(email: string, id: string) {
     if (!email && !id) return
-    return await this.prisma.user.create({
-      data: {
-        email,
-        req_id: id
+    try {
+      await this.prisma.user.create({
+        data: {
+          email,
+          req_id: id
+        }
+      })
+      return {
+        code: 200,
+        message: "Data saved in db"
       }
-    })
+    } catch (err) {
+      throw new InternalServerErrorException("Failed to saved data in the database...")
+    }
+
   }
   /**
    * Retrieves users from DB with pagination and optional filters
@@ -44,18 +54,29 @@ export class AppService {
    */
 
   async getDataDB(page: number, pageSize: number, filters?: { email?: string, req_id?: string }) {
-    return await this.prisma.user.findMany({
+    const where: Prisma.UserWhereInput = {
+      email: filters?.email ? { contains: filters.email, mode: 'insensitive' } : undefined,
+      req_id: filters?.req_id ? { equals: filters.req_id } : undefined,
+    }
+    const dataDB = await this.prisma.user.findMany({
       skip: (page - 1) * pageSize,
       take: pageSize,
-      where: {
-        email: filters?.email ? { contains: filters.email, mode: 'insensitive' } : undefined,
-        req_id: filters?.req_id ? { equals: filters.req_id } : undefined,
-      }, select: {
+      where, select: {
         id: true,
         email: true,
         req_id: true,
       }
     });
+    const totalResults = await this.prisma.user.count({ where })
+    const totalPages = Math.ceil(totalResults / pageSize)
+    return {
+      code: 200,
+      page,
+      pageSize,
+      totalResults,
+      totalPages,
+      data: dataDB
+    }
   }
 
   /**
@@ -65,8 +86,14 @@ export class AppService {
    */
 
   async getUserData(email: string) {
-    return await this.prisma.user.findMany({
+    const userData = await this.prisma.user.findMany({
       where: { email }
     })
+    if (userData.length === 0) throw new NotFoundException("Nenhum usuário encontrado com esse email")
+    return {
+      code: 200,
+      data: userData,
+      count: userData.length
+    }
   }
 }
